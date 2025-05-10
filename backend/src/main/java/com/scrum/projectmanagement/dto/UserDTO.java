d927e4772e0a01dcf37a57996b57b9898e7d0999
iMaDissame <topimad101@gmail.com>
2025-05-10 02:03:45 +0100

    User DTO config

---
+++ backend/src/main/java/com/scrum/projectmanagement/dto/UserDTO.java
@@ -0,0 +1,25 @@
+package com.scrum.projectmanagement.dto;
+
+import com.scrum.projectmanagement.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private User.Role role;
+
+    public static UserDTO fromEntity(User user) {
+        UserDTO dto = new UserDTO();
+        dto.setId(user.getId());
+        dto.setName(user.getName());
+        dto.setEmail(user.getEmail());
+        dto.setRole(user.getRole());
+        return dto;
+    }
+}
